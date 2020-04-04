fd1d03f8e7c77042a36579b96b56b42bc671ac78
HolyAngel <slverwolf@gmail.com>
2020-04-03 21:13:44 -0500

    include: blkdev.h: Do not collect I/O statistics
    This adds a great deal of latency to block requests due to disabling
    preemption temporarily during statistics collection.
    
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -602,7 +602,7 @@ struct request_queue {
 #define QUEUE_FLAG_NOWAIT       29	/* device supports NOWAIT */
 #define QUEUE_FLAG_SQ_SCHED     30	/* single queue style io dispatch */
 
-#define QUEUE_FLAG_MQ_DEFAULT	((1UL << QUEUE_FLAG_IO_STAT) |		\
+#define QUEUE_FLAG_MQ_DEFAULT	((0 << QUEUE_FLAG_IO_STAT) |		\
 				 (1UL << QUEUE_FLAG_SAME_COMP) |	\
 				 (1UL << QUEUE_FLAG_NOWAIT))
 
