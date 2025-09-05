9dabfa163a14f1e3d873608401a15d8e057f0f11
brokestar233 <3765589194@qq.com>
2025-09-06 02:35:11 +0800

    f2fs: Demote GC thread to idle scheduler class
    We don't want the background GC work causing UI jitter should it ever
    collide with periods of user activity.
    
    Change-Id: I6e7af55fcd62daf27f9ae7d6c7213adb991b73fb
    Signed-off-by: brokestar233 <3765589194@qq.com>
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -13,6 +13,7 @@
 #include <linux/delay.h>
 #include <linux/freezer.h>
 #include <linux/sched/signal.h>
+#include <uapi/linux/sched/types.h>
 #include <linux/random.h>
 #include <linux/sched/mm.h>
 
@@ -172,6 +173,7 @@ next:
 
 int f2fs_start_gc_thread(struct f2fs_sb_info *sbi)
 {
+	const struct sched_param param = { .sched_priority = 0 };
 	struct f2fs_gc_kthread *gc_th;
 	dev_t dev = sbi->sb->s_bdev->bd_dev;
 
@@ -202,6 +204,7 @@ int f2fs_start_gc_thread(struct f2fs_sb_info *sbi)
 	set_user_nice(gc_th->f2fs_gc_task,
 			PRIO_TO_NICE(sbi->critical_task_priority));
 
+	sched_setscheduler(sbi->gc_thread->f2fs_gc_task, SCHED_IDLE, &param);
 	set_task_ioprio(sbi->gc_thread->f2fs_gc_task,
 		IOPRIO_PRIO_VALUE(IOPRIO_CLASS_IDLE, 0));
 	return 0;
